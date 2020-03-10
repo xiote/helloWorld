0fc3978b4d35c76ff12a5ec5a1ddc55053b08763
xiote <xiote@nate.com>
2020-03-11 01:08:57 +0900

    test

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello2, " + txtName.text!
+    }
 
 }
 
